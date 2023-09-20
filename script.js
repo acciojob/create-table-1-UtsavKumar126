1df3f11adfdf59dc9b9ad2b1cc1a98af08b3555b
UtsavKumar126 <117590564+UtsavKumar126@users.noreply.github.com>
2023-09-20 15:17:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function insert_Row() {
     const newRow = document.createElement("tr");
 
     // Set the innerHTML of the new row with two cells
-    newRow.innerHTML = `<td>New cell1</td><td>New cell2</td>`;
+    newRow.innerHTML = `<td>New Cell1</td><td>New Cell2</td>`;
 
     // Get a reference to the table by its ID
     //const table1 = document.getElementById("sampleTable");
