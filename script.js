6a961c626b6a6db75ffd6d2c3101011f5ef5d03b
UtsavKumar126 <117590564+UtsavKumar126@users.noreply.github.com>
2023-09-20 14:30:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
+let curRow=3;
 function insert_Row() {
     //Write your code here
-  
-  
+  const newRow=document.createElement("tr");
+  	
+
+	newRow.innerHTML=`<td>Row${curRow} cell1</td>
+        <td>Row${curRow++} cell2</td>`
+	
+  const table1=document.getElementById("sampleTable")
+  table1.appendChild(newRow);	
+
+	
 }
